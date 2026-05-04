2b61f1f04adca4b5e08e741e5b34b2e5eebc52e3
mateu <mateuszjestemja90@gmail.com>
2026-05-04 23:17:03 +0200

    Fixed spelling error

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 SET PORT=%1
 IF "%PORT%"=="" SET PORT=8080
-SET PORT=%PORT% && docker compose up --build -d
+SET PORT=%PORT%
+docker compose up --build -d
 echo Stock Market API running at http://localhost:%PORT%
